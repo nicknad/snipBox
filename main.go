80bdbb6a723eef6f970c66fc4866f7fbbd24b296
nicknad <nadolskinick@gmail.com>
2024-07-26 17:14:35 +0200

    test air

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Go, We love nick  fas asdasfs"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet... !!"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new  snippet..."))
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
